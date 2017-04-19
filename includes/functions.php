56e2576b1d378650fd8083769f74674eb93e6cea
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-19 13:08:25 +0200

    from mysql to mysqli

--- includes/functions.php
+++ includes/functions.php
@@ -24,24 +24,13 @@
 	
 	//prepares queries before sending it to database
 	function mysql_prep( $value ) {
-		$magic_quotes_active = get_magic_quotes_gpc();
-		$new_enough_php = function_exists( "mysql_real_escape_string" ); // i.e. PHP >= v4.3.0
-		if( $new_enough_php ) { // PHP v4.3.0 or higher
-			// undo any magic quote effects so mysql_real_escape_string can do the work
-			if( $magic_quotes_active ) { $value = stripslashes( $value ); }
-			$value = mysql_real_escape_string( $value );
-		} else { // before PHP v4.3.0
-			// if magic quotes aren't already on then add slashes manually
-			if( !$magic_quotes_active ) { $value = addslashes( $value ); }
-			// if magic quotes are active, then the slashes already exist
-		}
-		return $value;
+		return mysqli_real_escape_string( $value );
 	}
 
 	//check if the query went well
 	function confirm_query($result_set) {
 		if (!$result_set) {
-			die("Database query failed: " . mysql_error());
+			die("Database query failed");
 		}
 	}
 ?>
