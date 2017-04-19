cc2cf66b7fc02b0b4e241a3e81e8aad693d5705e
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-19 12:14:15 +0200

    db connction functions
    file created includes db connction functions

---
+++ includes/db_connection.php
@@ -0,0 +1,25 @@
+<?php
+require("db_constants.php");
+
+//Connect to database server
+//and Select the database 
+function db_connect(){
+	//Create a database connection
+	$connection = mysql_connect(DB_SERVER,DB_USER,DB_PASS);
+	if (!$connection) {
+		die("Database connection failed: " . mysql_error());
+	}
+
+	//Select a database to use 
+	$db_select = mysql_select_db(DB_NAME,$connection);
+	if (!$db_select) {
+		die("Database selection failed: " . mysql_error());
+	}	
+}
+
+//Close connection
+function db_disconnect(){
+	mysql_close($connection);
+}
+
+?>
