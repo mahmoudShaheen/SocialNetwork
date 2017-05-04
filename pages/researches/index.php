b66d49afd4985f019bdb4f70e4d7df4403bd21d7
AhmedBadee <ahmedbadee17@gmail.com>
2017-05-04 16:36:44 +0200

    Researches

---
+++ pages/researches/index.php
@@ -0,0 +1,33 @@
+<?php require_once("../../includes/researches_includes/statement_functions.php"); ?>
+<?php require_once("../../includes/session.php"); ?>
+<?php
+	require_once("../../includes/db_connection.php");
+	global $connection;
+?>
+<?php require_once("../../includes/functions.php"); ?>
+
+<?php
+	confirm_logged_in();
+?>
+
+<?php
+
+    $all_researches = read_all_researches($read_all_researches_stmt);
+
+    while ($one_research = $all_researches->fetch_array(MYSQLI_NUM)) {
+
+		$idResearches    = $one_research[0];
+		$user_id         = read_user_research($user_research_stmt);
+
+		$user_data       = read_user_by_id($read_user_stmt);
+		$user_data_array = $user_data->fetch_array(MYSQLI_NUM);
+
+		// User data for this research
+		echo htmlentities($user_data_array[1]);           // First name entry
+		echo htmlentities($user_data_array[3]);           // Last name entry
+
+		// Research data
+        echo htmlentities($one_research[1]);        // Title entry
+        echo htmlentities($one_research[2]);        // Idea entry
+    }
+?>
