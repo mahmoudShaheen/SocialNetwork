3aa182ba6e5e4f1da0043c91a44525251faa91ab
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-05-06 13:17:26 +0200

    component, lab pages/functions added

---
+++ pages/lab/add-edit.php
@@ -0,0 +1,78 @@
+<?php
+	require_once("../../includes/labs_includes/statement_functions.php");
+	global $insert_lab_stmt;
+	global $update_lab_stmt;
+?>
+<?php require_once("../../includes/session.php"); ?>
+<?php
+	require_once("../../includes/db_connection.php");
+	global $connection;
+?>
+<?php require_once("../../includes/functions.php"); ?>
+
+<?php
+	confirm_logged_in();
+	confirm_admin();
+?>
+
+<?php include("../../includes/header_admin.php") ?>
+<?php include("../../includes/sidebar_admin.php"); ?>
+
+<?php
+	include_once("../../includes/form_functions.php");
+
+    if ($_GET['id']) {
+
+	    $lab_id   = $_GET['id'];
+
+		// Old lab data to update
+		$lab_data = read_lab_by_id($read_lab_stmt);
+
+		echo htmlentities($lab_data[1]);        // LabName entry
+        echo htmlentities($lab_data[2]);        // Contents entry
+        echo htmlentities($lab_data[3]);        // Place entry
+        echo htmlentities($lab_data[4]);        // About entry
+        echo htmlentities($lab_data[5]);        // Picture entry
+    }
+
+	if ($_POST && $_GET['id']) {
+		form_validate($lab_id);
+	} else if ($_POST) {
+		form_validate();
+	}
+
+ ?>
+ <?php include("../../includes/footer_admin.php") ?>
+
+<?php
+
+	function form_validate($lab_id = NULL) {
+		$errors = array();
+
+		// perform validations on the form data
+		$required_fields    = array('lab_name', 'contents', 'place');
+		$errors             = array_merge($errors, check_required_fields($required_fields));
+		$fields_max_lengths = array('lab_name' => 45, 'contents' => 45, 'place' => 45);
+		$errors             = array_merge($errors, check_max_field_lengths($fields_max_lengths));
+		$fields_min_lengths = array('lab_name' => 8, 'contents' => 8, 'place' => 8);
+		$errors             = array_merge($errors, check_min_field_lengths($fields_min_lengths));
+
+		// New lab data
+		$lab_name = trim($_POST['lab_name']);                    // name for html input tag => title
+		$contents = trim($_POST['contents']);                    // name for html input tag => idea
+		$place    = trim($_POST['place']);                       // name for html input tag => place
+		$about    = trim($_POST['about']);                       // name for html input tag => about
+		$picture  = trim($_POST['picture']);                     // name for html input tag => picture
+
+		if (empty($errors)) {
+			if ($lab_id != NULL) {
+				execute_stmt($update_lab_stmt);
+				close_stmt($update_lab_stmt);
+			} else {
+				execute_stmt($insert_lab_stmt);
+				close_stmt($insert_lab_stmt);
+			}
+		}
+	}
+
+ ?>
---
+++ pages/lab/index.php
@@ -0,0 +1,41 @@
+<?php require_once("../../includes/labs_includes/statement_functions.php"); ?>
+<?php require_once("../../includes/session.php"); ?>
+<?php
+	require_once("../../includes/db_connection.php");
+	global $connection;
+?>
+<?php require_once("../../includes/functions.php"); ?>
+
+<?php
+	confirm_logged_in();
+?>
+<?php
+	if(admin_check()){ //user is admin
+		include("../../includes/header_admin.php");
+		include("../../includes/sidebar_admin.php");
+	}else{ //normal user
+		include("../../includes/header.php");
+		include("../../includes/sidebar.php");
+	}
+?>
+
+<?php
+
+    $all_labs = read_all_labs($read_all_labs_stmt);
+
+    while ($one_lab = $all_labs->fetch_array(MYSQLI_NUM)) {
+        echo "$one_lab[1] </br>";        // LabName entry
+        echo "$one_lab[2] </br>";        // Contents entry
+        echo "$one_lab[3] </br>";        // Place entry
+        echo "$one_lab[4] </br>";        // About entry
+        echo "$one_lab[5] </br>";        // Picture entry
+    }
+
+ ?>
+<?php
+	if(admin_check()){ //user is admin
+		include("../../includes/footer_admin.php");
+	}else{ //normal user
+		include("../../includes/footer.php");
+	}
+?>
