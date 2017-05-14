ac50c52668344db3ec089627b59294262f285623
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-05-14 19:28:23 +0200

    merge includes folder

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,78 @@
 
 		</div>
-		<div id="footer">Copyright 2017, CSE ZU</div>
+		<div class="footer" id="contact">
+			            <div class="infoo">
+				            <div class="col-xs-12 col-md-3 col-lg-3">
+				            	<ul>
+				            		<li><a href="course.html"><h2>Services</h2></a></li>
+				            		<li><a href="course.html"><p>Embedded</p> </a></li>
+				            		<li><a href="course.html"><p>Software</p> </a></li>
+				            		<li><a href="course.html"><p><p>Embedded</p></p> </a></li>
+				            		<li><a href="course.html"><p>Software</p></a></li>
+				            		<li><a href="course.html"><p>Embedded</p></a></li>
+				            		<li><a href="course.html"><p>Software</p></a></li>
+				            	</ul>
+				            </div>
+			          	</div>
+			          	<form class="form-horizontal col-xs-12 col-md-3 col-lg-3">
+							  <div class="form-group">
+							    
+							      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+							    
+							  </div>
+							  <div class="form-group">
+							    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">				     
+							  </div>
+							  <button type="submit" class="btn btn-default">Submit</button>
+						</form>
+	          	
+	               		<div class="col-xs-12 col-md-3 col-lg-3 lists">
+	               			<ul>
+				              	<li> <h2>Contact Me</h2></li>
+				              	<li><p>Email:enghkho@gmail.com</p></li>
+				              	<li><p>Email:enghkho@ieee-zsb.org</p></li>
+				              	<li><p>Phone:+201000484292</p></li>
+				              	<li><p>Copyright 2017 </p></li>			                
+				            </ul>
+				        </div>
+			            <div class="col-xs-12 col-md-3 col-lg-3 icons">
+			                <ul>
+				                <li>
+				                    <a class="ex1" href="https://www.facebook.com/hend.osman.984">
+				                        <i class="fa fa-facebook"></i>
+				                    </a>
+			                    </li>
+			                    <li>
+				                    <a class="ex2" href="https://twitter.com/enghkho">
+				                        <i class="fa fa-twitter"></i>
+				                    </a>
+			                    </li>
+			                    <li>
+				                    <a class="ex3" href="https://plus.google.com/u/1/">
+				                        <i class="fa fa-google-plus"></i>
+				                    </a>
+			                    </li>
+			                    <li>
+				                    <a class="ex4" href="https://www.youtube.com/watch?v=yoAlmcBmoys">
+				                        <i class="fa fa-youtube"></i>
+				                    </a>
+			                    </li>
+			                    <li>
+				                    <a class="ex5" href="https://goo.gl/LZlebV">
+				                        <i class="fa fa-linkedin"></i>
+				                    </a>
+			                    </li>
+			                    <li>
+				                    <a class="ex6" href="https://soundcloud.com/eng-h-khairy-osman">
+				                        <i class="fa fa-soundcloud"></i>
+				                    </a>
+			                    </li>
+		                    </ul> 
+		                </div>
+           			</div>
+		
+		<script src="../../javascripts/jquery-2.2.0.min.js"></script>
+    	<script src="../../javascripts/bootstrap.min.js"></script>
 	</body>
 </html>
 
